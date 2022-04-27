5081f571a245b7f0d675f6b69c9453bb2030fed7
Gabriel Ekblad <gabriel@ekblad.cc>
2022-04-27 08:19:33 +0200

    optimized replace function for list

--- src/main/kotlin/com/github/ekenstein/sgf/utils/ListUtils.kt
+++ src/main/kotlin/com/github/ekenstein/sgf/utils/ListUtils.kt
@@ -2,7 +2,11 @@ package com.github.ekenstein.sgf.utils
 
 import java.lang.Integer.min
 
-fun <T> List<T>.replace(index: Int, with: T): List<T> = subList(0, index) + with + subList(min(size, index + 1), size)
+fun <T> List<T>.replace(index: Int, with: T): List<T> = when (index) {
+    size - 1 -> replaceLast(with)
+    0 -> replaceFirst(with)
+    else -> subList(0, index) + with + subList(min(size, index + 1), size)
+}
 fun <T> List<T>.replaceLast(with: T): List<T> = take(size - 1) + with
-fun <T> List<T>.replaceFirst(with: T): List<T> = listOf(with) + drop(1)
+fun <T> List<T>.replaceFirst(with: T): List<T> = drop(1).prepend(with)
 fun <T> List<T>.prepend(item: T) = listOf(item) + this
